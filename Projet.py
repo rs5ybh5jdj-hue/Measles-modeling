ca3423b78cfa9bb397727525b87e7d110e22f3cc
joadam02-svg <jo.adam02@hotmail.com>
2025-11-19 16:51:46 +0100

    update

--- Projet.py
+++ Projet.py
@@ -10,28 +10,27 @@ import matplotlib.pyplot as plt
 ## Reported cases of measles
 # World Health Organization
 reported_cases_WHO = pd.read_csv("C:/Users/Joséphine/OneDrive - UCL/Master 1/Process modelling/Projet - measles/Measles reported cases WHO.csv", encoding='utf-8', sep=";")
-print(reported_cases_WHO.head())
 
 # Our World in Data
 reported_cases_OWID = pd.read_csv("C:/Users/Joséphine/OneDrive - UCL/Master 1/Process modelling/Projet - measles/reported-cases-of-measles OWD.csv")
 
 cases_OWID = reported_cases_OWID.drop(columns = ['Entity','Code'], axis=1) # "axis=1" = columns
-print(cases_OWID.head())
 
 ## Vaccination
 # World Health Organization
 vaccin = pd.read_csv("C:/Users/Joséphine/OneDrive - UCL/Master 1/Process modelling/Projet - measles/Measles vaccination coverage WHO.csv", sep=";")
-print(vaccin)
 
 columns_to_drop = ['GROUP', 'CODE', 'NAME', 'ANTIGEN_DESCRIPTION', 'COVERAGE_CATEGORY', 'COVERAGE_CATEGORY_DESCRIPTION']
 
 vaccin_cleaned = vaccin.drop(columns=columns_to_drop, axis=1)
 
 mcv1 = vaccin_cleaned[vaccin_cleaned['ANTIGEN'] == 'MCV1']
-print(mcv1.head())
 
 mcv2 = vaccin_cleaned[vaccin_cleaned['ANTIGEN'] == 'MCV2']
-print(mcv2.head())
+
+## Population growth
+# Our World in Data
+pop_growth = pd.read_csv("C:/Users/Joséphine/OneDrive - UCL/Master 1/Process modelling/Projet - measles/population-growth-rates.csv")
 
 # ---------------------------------------------------------------------------
 ### PARAMETERS ###
@@ -42,5 +41,10 @@ gamma = 1/5
 
 # Incubation period
 sigma = 1/12
- 
 
+# Transmission rate
+beta = 2.5 # à revoir
+
+# Natality and mortality
+mu = pop_growth['Population growth rate - Sex: all - Age: all - Variant: estimates']
+print(mu.head())
